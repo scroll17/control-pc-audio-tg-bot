f4fc724b1db247d1378b1580a750ebb1c00b6cd4
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2023-10-07 15:19:17 +0300

    modules/telegram/common/filters: update TelegrafException filter

--- src/modules/telegram/common/filters/telegraf-exception.filter.ts
+++ src/modules/telegram/common/filters/telegraf-exception.filter.ts
@@ -1,10 +1,14 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import {ArgumentsHost, Catch, ExceptionFilter, Logger} from '@nestjs/common';
 import { TelegrafArgumentsHost } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
 
 @Catch()
 export class TelegrafExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(this.constructor.name);
+
   async catch(exception: Error, host: ArgumentsHost): Promise<void> {
+    this.logger.error('Catch exception', exception)
+
     const telegrafHost = TelegrafArgumentsHost.create(host);
     const ctx = telegrafHost.getContext<Context>();
 
