99a017bbe7cb540db577eadec081b9d1355e7fe2
Denys Zolotarenko <zololotarenko.2015@gmail.com>
2023-10-07 15:18:17 +0300

    modules/integrations: implement module

--- src/modules/integrations/integration.module.ts
+++ src/modules/integrations/integration.module.ts
@@ -2,11 +2,12 @@
 import { Module } from '@nestjs/common';
 /*services*/
 import { IntegrationService } from './integration.service';
+import {MacOSAudioController} from "./os/MacOSAudioController";
 /*controllers*/
 /*@entities*/
 
 @Module({
-  providers: [IntegrationService],
+  providers: [IntegrationService, MacOSAudioController],
   exports: [IntegrationService],
 })
 export class IntegrationModule {}
--- src/modules/integrations/integration.service.ts
+++ src/modules/integrations/integration.service.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 /*modules*/
 /*adapters*/
 /*providers*/
+import {MacOSAudioController} from "./os/MacOSAudioController";
 /*common*/
 /*libs*/
 /*db*/
@@ -13,5 +14,34 @@ import { ConfigService } from '@nestjs/config';
 export class IntegrationService {
   private readonly logger = new Logger(this.constructor.name);
 
-  constructor(private configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly macOSAudioController: MacOSAudioController
+  ) {
+    // TODO: add abstraction for each OS
+  }
+
+  public async play() {
+    await this.macOSAudioController.play()
+  }
+
+  public async pause() {
+    await this.macOSAudioController.pause()
+  }
+
+  public async togglePlayPause() {
+    await this.macOSAudioController.togglePlayPause()
+  }
+
+  public async next() {
+    await this.macOSAudioController.next()
+  }
+
+  public async prev() {
+    await this.macOSAudioController.prev()
+  }
+
+  public async getInfo(): Promise<Record<string, unknown>> {
+    return this.macOSAudioController.getInfo()
+  }
 }
